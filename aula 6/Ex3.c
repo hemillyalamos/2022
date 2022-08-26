24d6661722d8acbd23c1976867d8ff6cb0f0f581
hemillyalamos <hemilly27.nicole@gmail.com>
2022-08-26 08:48:29 -0300

    ds

--- aula 6/Ex3.c
+++ aula 6/Ex3.c
@@ -1,23 +1,23 @@
-/*3 - Utilizando apenas um vetor de tamanho 6, troque os valores de maneira inversa*/
+//3 - Utilizando apenas um vetor de tamanho 6, troque os valores de maneira inversa//
 
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
 	setlocale(LC_ALL,"");
-	int vet[6],num,cont=0,i;	
+	int vetor[6],number,cont=0,i;	
 	
 	printf("Vetor 1");
 	for(i = 0; i <= 5 ; i++)
 {
 	cont=0+i;
 	printf("\t[%d] Digite um valor: ",cont);
-	scanf("%d",&num);
-		vet[i] = num;
+	scanf("%d",&number);
+		vetor[i] = number;
 }
 	printf("vetor 1:\n");
 	for(i=5; i>=0;i--)
-		printf("%d ", vet[i]);
+		printf("%d ", vetor[i]);
 		printf("\n");
 
 }
