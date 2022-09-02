614fc98771eb648b3fe2961e31cd26360dd970d8
hemillyalamos <hemilly27.nicole@gmail.com>
2022-09-02 07:30:54 -0300

    ds

--- ead/ex7.c
+++ ead/ex7.c
@@ -6,27 +6,31 @@ int main()
     int positivos = 0;
 
     printf("Telefonou para a vitima? (Sim ou Nao): ");
-    scanf("%c", &resposta);
+    scanf("%s", &resposta);
     if (resposta == 'Sim' || resposta == 'Sim')
         positivos++;
+        
     printf("Esteve no local do crime? (Sim ou Nao): ");
     getchar();
-    scanf("%c", &resposta);
+    scanf("%s", &resposta);
     if (resposta == 'Sim' || resposta == 'Sim')
         positivos++;
+        
     printf("Mora perto da vitima? (Sim ou Nao): ");
     getchar();
-    scanf("%c", &resposta);
+    scanf("%s", &resposta);
     if (resposta == 'Sim' || resposta == 'Sim')
         positivos++;
+        
     printf("Devia para a vitima? (Sim ou Nao): ");
     getchar();
-    scanf("%c", &resposta);
+    scanf("%s", &resposta);
     if (resposta == 'Sim' || resposta == 'Sim')
         positivos++;
+        
     printf("Ja trabalhou com a vitima? (Sim ou Nao): ");
     getchar();
-    scanf("%c", &resposta);
+    scanf("%s", &resposta);
     if (resposta == 'Sim' || resposta == 'Sim')
         positivos++;
 
