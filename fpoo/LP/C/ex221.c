95c34ff3e694eff38ab5e74d4f7fadfe3566e431
hemillyalamos <hemilly27.nicole@gmail.com>
2022-09-22 11:20:44 -0300

    ds

---
+++ fpoo/LP/C/ex221.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+       int n, c, l;
+       printf("Digite o Tamanho: ");
+       scanf("%d", &n);
+   
+       for (l=1;l<=n;l++) {
+        for(c=1;c<=l;c++)
+         printf("*");
+       printf("\n");
+       }
+       getch();    
+}
