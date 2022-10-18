1d5d2bb8467c2755a462181d6072119fc48ef9d1
hemillyalamos <hemilly27.nicole@gmail.com>
2022-10-18 07:35:56 -0300

    ds

--- Java/Pacientes/Pacientes/src/pac/Main.java
+++ Java/Pacientes/Pacientes/src/pac/Main.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		nome paciente [] = new nome[10];
-		
-		
-		
 		Scanner scan = new Scanner(System.in);
+		Paciente pacientes[] = new Paciente[10];
 		
-		System.out.println("Digite o nome do paciente:");
-		paciente = scan.next();
+		//Entrada
+		System.out.println("Digite 10 nomes, pesos e alturas:");
+		for(int i = 0; i < 10; i++)
+			pacientes[i] = new Paciente(scan.next(), scan.nextFloat(), scan.nextFloat());
 		
-
-	}
-
+		//Saída
+	System.out.println("Nome\tPeso\tAltura\tIMC\tDiagnóstico:");
+	for(int i = 0; i < 10; i++)
+		System.out.println(pacientes[i].toString());
+	totImc += pacientes[i].imc();
+	totPeso += pacientes[i].peso;
+	totAltura += pacientes[i].altura;
+}
+float mediaImc = totImc / 10f;
+float mediaPeso = totPeso / 10f;
+float mediaAltura = totAltura / 10f;
+System.out.println("Estatísticas:");
+System.out.printf("Média IMC = %.2f\n",mediaImc);
+System.out.printf("Média Peso = %.2f\n",mediaPeso);
+System.out.printf("Média Altura = %.2f\n",mediaAltura);
+	
 }
